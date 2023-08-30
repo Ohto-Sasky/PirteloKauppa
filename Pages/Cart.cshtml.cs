de9b294b9a863bd064e9fa5736c7b144d2570bc8
JesperiKulo <141813181+JesperiKulo@users.noreply.github.com>
2023-08-30 09:55:49 +0200

    Create Cart.cshtml.cs

---
+++ Pages/Cart.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PirteloKauppa.Pages
+{
+    public class MenuModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
